1a4dbaa1b5efe3ed547fff37b771ae64357d4211
Jibon L. Costa <jiboncosta57@gmail.com>
2026-09-10 13:45:13 +0600

    bump

--- administrator/components/com_plugnmeet/src/Helper/libs/packages/vendor/composer/installed.php
+++ administrator/components/com_plugnmeet/src/Helper/libs/packages/vendor/composer/installed.php
@@ -1,19 +1,19 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => 'dev-main',
-        'version' => 'dev-main',
-        'reference' => '57f234d4eb8fa28b2c3b4176793d8b2fd2e4b9f1',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'dev' => true,
+        'dev' => false,
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => 'dev-main',
-            'version' => 'dev-main',
-            'reference' => '57f234d4eb8fa28b2c3b4176793d8b2fd2e4b9f1',
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
